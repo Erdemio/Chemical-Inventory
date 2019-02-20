a16fb8fed3c68e3a454b5c5c00b3625e190bcb4e
Erdemio <erdem_arslan_694@hotmail.com>
2019-02-20 12:19:27 +0300

    Sistem iyileştirme

--- index.php
+++ index.php
@@ -22,13 +22,13 @@ require_once "procedure.php";
           <div class="nav-wrapper">
             <div class="row">
               <div class="col m8  hide-on-med-and-down">
-                <a href="#!" class="breadcrumb">First</a>
-                <a href="#!" class="breadcrumb">Second</a>
-                <a href="#!" class="breadcrumb">Third</a>
+                <a href="index" class="breadcrumb">Anasayfa</a>
+                <a href="#!" class="breadcrumb">Alt Sayfa</a>
+                <a href="#!" class="breadcrumb">Alt Sayfa</a>
               </div>
               <div class="col m4 s12">
                 <a href="#" data-target="nav-mobile" class=" sidenav-trigger full hide-on-large-only"><i class="material-icons">menu</i></a>
-                <a href="logoff" class="right"><i class="material-icons">exit_to_app</i></a>
+                <a  href="#logoff" class="right modal-trigger"><i class="material-icons">exit_to_app</i></a>
               </div>
             </div>
           </div>
@@ -38,15 +38,15 @@ require_once "procedure.php";
             <a href="index"><img class="profil" src="assets/deu.png"></a>
             <span class="userid"><?php echo @$_SESSION['user']; ?></span>
           </div></li>
-          <li><a href="#!" class="waves-effect"><i class="material-icons">cloud</i>1</a></li>
+          <li><a href="#!" class="waves-effect"><i class="material-icons">cloud</i>Kimyasal Listele</a></li>
           <li><div class="divider"></div></li>
-          <li><a href="#!" class="waves-effect"><i class="material-icons">cloud</i>2</a></li>
-          <li><a href="#!" class="waves-effect"><i class="material-icons">cloud</i>3</a></li>
-          <li><a href="#!" class="waves-effect"><i class="material-icons">cloud</i>4</a></li>
+          <li><a href="#!" class="waves-effect"><i class="material-icons">cloud</i>Kimyasal Düzenle</a></li>
+          <li><a href="#!" class="waves-effect"><i class="material-icons">cloud</i>Kimyasal Ekle</a></li>
+          <li><a href="#!" class="waves-effect"><i class="material-icons">cloud</i>Dışarı Aktar</a></li>
           <li><a href="#!" class="waves-effect"><i class="material-icons">cloud</i>5</a></li>
           <li><div class="divider"></div></li>
           <li><a href="#!" class="waves-effect"><i class="material-icons">cloud</i>6</a></li>
-          <li><a href="logoff" class="waves-effect"><i class="material-icons">exit_to_app</i>Çıkış Yap</a></li>
+          <li><a href="#logoff" class="waves-effect modal-trigger"><i class="material-icons">exit_to_app</i>Çıkış Yap</a></li>
         </ul>
       </header>
       <main>
@@ -58,13 +58,30 @@ require_once "procedure.php";
           </div>
         </div>
       </main>
+
+      <div id="logoff" class="modal mlogoff">
+        <div class="modal-content">
+          <h4>Çıkış yap</h4>
+          <p>Güvenli çıkış yapmak için Evet'e tıklayın.</p>
+        </div>
+        <div class="modal-footer">
+          <a href="javascript:void(0)" class="modal-close btn waves-effect waves-light">Geri dön</a>
+          <a href="logoff" class="btn waves-effect red waves-light">Evet<i class="material-icons right">exit_to_app</i></a>
+        </div>
+      </div>
+
       <script type="text/javascript" src="js/materialize.min.js"></script>
       <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
       <script type="text/javascript">
         document.addEventListener('DOMContentLoaded', function() {
-          var options;
+        var options;
         var elems = document.querySelectorAll('.sidenav');
         var instances = M.Sidenav.init(elems, options);
+        });
+        document.addEventListener('DOMContentLoaded', function() {
+          var options;
+          var elems = document.querySelectorAll('.modal');
+          var instances = M.Modal.init(elems, options);
         });
       </script>
     </body>
